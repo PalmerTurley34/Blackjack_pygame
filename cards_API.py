6c889340e73402b230b623da796b06a6134565e4
PalmerTurley34 <palmerturley34@gmail.com>
2021-08-13 18:25:23 -0600

    create buttons

--- cards_API.py
+++ cards_API.py
@@ -1,7 +1,6 @@
 import requests
 from constants import API_URL
 
-deck_id = requests.get(f'{API_URL}new/shuffle/?deck_count=1').json()['deck_id']
 
 class Deck():
     def __init__(self, deck_id):
@@ -10,7 +9,7 @@ class Deck():
 
     def draw(self, character):
         card = requests.get(f'{API_URL}{self.deck_id}/draw/?count=1').json()['cards'][0]
-        requests.get(f'{API_URL}{deck_id}/pile/{character.name}/add/?cards={card["code"]}')
+        requests.get(f'{API_URL}{self.deck_id}/pile/{character.name}/add/?cards={card["code"]}')
         character.cards.append(card)
         
         
@@ -19,7 +18,7 @@ class Deck():
         requests.get(f'{API_URL}{self.deck_id}/shuffle/')
 
     def show_pile(self, character):
-        return requests.get(f'{API_URL}{deck_id}/pile/{character.name}/list/').json()
+        return requests.get(f'{API_URL}{self.deck_id}/pile/{character.name}/list/').json()
 
 class Player():
     def __init__(self, name):
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 from constants import *
+from cards_API import *
 
 pygame.init()
 
 pygame.display.set_caption('Blackjack')
+clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 while True:
+    mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    pygame.draw.rect(screen, 'green', pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.flip()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if 0 <= mouse[0] <= 50 and 0 <= mouse[1] <= 50:
+                pygame.quit()
+
+    pygame.draw.rect(screen, 'green', pygame.Rect(0, 0, 50, 50))
+
+# initiate the deck and players
+    deck_id = requests.get(f'{API_URL}new/shuffle/?deck_count=1').json()['deck_id']
+    deck = Deck(deck_id)
+    player = Player('Palmer')
+    dealer = Dealer()
+
+
+    pygame.display.update()
+    clock.tick(60)
             
